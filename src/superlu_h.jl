c9b61f2270005681cd509ec3de4dc9e2929cfcaf
Douglas Bates <dmbates@gmail.com>
2013-12-12 13:35:53 -0600

    Update to version 4.3 of SuperLU

--- src/superlu_h.jl
+++ src/superlu_h.jl
@@ -13,17 +13,30 @@
 const EMPTY = int32(-1)
 const FALSE = int32(0)
 const TRUE = int32(1)
+const NO_MEMTYPE = int32(4)
+
+const NODROP = (0x0000)
+const DROP_BASIC = (0x0001)
+const DROP_PROWS = (0x0002)
+const DROP_COLUMN = (0x0004)
+const DROP_AREA = (0x0008)
+const DROP_SECONDARY = (0x000E)
+const DROP_DYNAMIC = (0x0010)
+const DROP_INTERP = (0x0100)
+const MILU_ALPHA = (1.0e-2)
 
 typealias int_t Cint
 
 # begin enum Stype_t
 typealias Stype_t Cint   # technically, Uint32 but printing a Cint is less verbose
 const SLU_NC = int32(0)
-const SLU_NR = int32(1)
-const SLU_SC = int32(2)
-const SLU_SR = int32(3)
-const SLU_NCP = int32(4)
-const SLU_DN = int32(5)
+const SLU_NCP = int32(1)
+const SLU_NR = int32(2)
+const SLU_SC = int32(3)
+const SLU_SCP = int32(4)
+const SLU_SR = int32(5)
+const SLU_DN = int32(6)
+const SLU_NR_loc = int32(7)
 # end enum Stype_t
 
 # begin enum Dtype_t
@@ -69,6 +82,7 @@ type NRformat
     colind::Ptr{int_t}
     rowptr::Ptr{int_t}
 end
+
 type SCformat
     nnz::int_t
     nsuper::int_t
@@ -78,6 +92,19 @@ type SCformat
     rowind_colptr::Ptr{int_t}
     col_to_sup::Ptr{int_t}
     sup_to_col::Ptr{int_t}
+end
+type SCPformat
+    nnz::int_t
+    nsuper::int_t
+    nzval::Ptr{None}
+    nzval_colbeg::Ptr{int_t}
+    nzval_colend::Ptr{int_t}
+    rowind::Ptr{int_t}
+    rowind_colbeg::Ptr{int_t}
+    rowind_colend::Ptr{int_t}
+    col_to_sup::Ptr{int_t}
+    sup_to_colbeg::Ptr{int_t}
+    sup_to_colend::Ptr{int_t}
 end
 type NCPformat
     nnz::int_t
@@ -89,6 +116,14 @@ end
 type DNformat
     lda::int_t
     nzval::Ptr{None}
+end
+type NRformat_loc
+    nnz_loc::int_t
+    m_loc::int_t
+    fst_row::int_t
+    nzval::Ptr{None}
+    rowptr::Ptr{int_t}
+    colind::Ptr{int_t}
 end
 
 # begin enum yes_no_t
@@ -118,7 +153,10 @@ const NATURAL = int32(0)
 const MMD_ATA = int32(1)
 const MMD_AT_PLUS_A = int32(2)
 const COLAMD = int32(3)
-const MY_PERMC = int32(4)
+const METIS_AT_PLUS_A = int32(4)
+const PARMETIS = int32(5)
+const ZOLTAN = int32(6)
+const MY_PERMC = int32(7)
 # end enum colperm_t
 
 # begin enum trans_t
@@ -139,9 +177,9 @@ const BOTH = int32(3)
 # begin enum IterRefine_t
 typealias IterRefine_t Cint
 const NOREFINE = int32(0)
-const SINGLE = int32(1)
-const DOUBLE = int32(2)
-const EXTRA = int32(3)
+const SLU_SINGLE = int32(1)
+const SLU_DOUBLE = int32(2)
+const SLU_EXTRA = int32(3)
 # end enum IterRefine_t
 
 # begin enum MemType
@@ -150,6 +188,8 @@ const LUSUP = int32(0)
 const UCOL = int32(1)
 const LSUB = int32(2)
 const USUB = int32(3)
+const LLVL = int32(4)
+const ULVL = int32(5)
 # end enum MemType
 
 # begin enum stack_end_t
@@ -158,25 +198,46 @@ const HEAD = int32(0)
 const TAIL = int32(1)
 # end enum stack_end_t
 
+# begin enum LU_space_t
 typealias LU_space_t Cint
 const SYSTEM = int32(0)
 const USER = int32(1)
 # end enum LU_space_t
 
+# begin enum norm_t
+typealias norm_t Cint
+const ONE_NORM = int32(0)
+const TWO_NORM = int32(1)
+const INF_NORM = int32(2)
+# end enum norm_t
+
+# begin enum milu_t
+typealias milu_t Cint
+const SILU = int32(0)
+const SMILU_1 = int32(1)
+const SMILU_2 = int32(2)
+const SMILU_3 = int32(3)
+# end enum milu_t
+
 # begin enum PhaseType
 typealias PhaseType Cint
 const COLPERM = int32(0)
-const RELAX = int32(1)
-const ETREE = int32(2)
-const EQUIL = int32(3)
-const FACT = int32(4)
-const RCOND = int32(5)
-const SOLVE = int32(6)
-const REFINE = int32(7)
-const TRSV = int32(8)
-const GEMV = int32(9)
-const FERR = int32(10)
-const NPHASES = int32(11)
+const ROWPERM = int32(1)
+const RELAX = int32(2)
+const ETREE = int32(3)
+const EQUIL = int32(4)
+const SYMBFAC = int32(5)
+const DIST = int32(6)
+const FACT = int32(7)
+const COMM = int32(8)
+const SOL_COMM = int32(9)
+const RCOND = int32(10)
+const SOLVE = int32(11)
+const REFINE = int32(12)
+const TRSV = int32(13)
+const GEMV = int32(14)
+const FERR = int32(15)
+const NPHASES = int32(16)
 # end enum PhaseType
 
 typealias flops_t Cfloat
@@ -187,15 +248,43 @@ type superlu_options_t
     ColPerm::colperm_t
     Trans::trans_t
     IterRefine::IterRefine_t
-    PrintStat::yes_no_t
-    SymmetricMode::yes_no_t
     DiagPivotThresh::Cdouble
+    SymmetricMode::yes_no_t
     PivotGrowth::yes_no_t
     ConditionNumber::yes_no_t
     RowPerm::rowperm_t
+    ILU_DropRule::Cint
+    ILU_DropTol::Cdouble
+    ILU_FillFactor::Cdouble
+    ILU_Norm::norm_t
+    ILU_FillTol::Cdouble
+    ILU_MILU::milu_t
+    ILU_MILU_Dim::Cdouble
+    ParSymbFact::yes_no_t
     ReplaceTinyPivot::yes_no_t
     SolveInitialized::yes_no_t
     RefineInitialized::yes_no_t
+    PrintStat::yes_no_t
+    nnzL::Cint
+    nnzU::Cint
+    num_lookaheads::Cint
+    lookahead_etree::yes_no_t
+    SymPattern::yes_no_t
+end
+type e_node
+    size::Cint
+    mem::Ptr{None}
+end
+type ExpHeader
+    size::Cint
+    mem::Ptr{None}
+end
+type LU_stack_t
+    size::Cint
+    used::Cint
+    top1::Cint
+    top2::Cint
+    array::Ptr{None}
 end
 type SuperLUStat_t
     panel_histo::Ptr{Cint}
@@ -203,6 +292,11 @@ type SuperLUStat_t
     ops::Ptr{flops_t}
     TinyPivots::Cint
     RefineSteps::Cint
+    expansions::Cint
+end
+type mem_usage_t
+    for_lu::Cfloat
+    total_needed::Cfloat
 end
 type GlobalLU_t
     xsup::Ptr{Cint}
@@ -219,9 +313,7 @@ type GlobalLU_t
     nzlumax::Cint
     n::Cint
     MemModel::LU_space_t
-end
-type mem_usage_t
-    for_lu::Cfloat
-    total_needed::Cfloat
-    expansions::Cint
+    num_expansions::Cint
+    expanders::Ptr{ExpHeader}
+    stack::LU_stack_t
 end
